518f145ac23de5315909325841ec86210153e91c
Skips2003 <jonnyforbes0805@gmail.com>
2026-03-23 18:59:21 +0000

    created loadWorkoutsFromSchedule function, i thought we will need it on a few diffferent pages and it would get a bit repetitive.

--- app/main/routes.py
+++ app/main/routes.py
@@ -6,7 +6,7 @@ from app.main import bp
 from app import db, bcrypt, loginManager
 import requests
 
-daysOfTheWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+daysOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
 def searchExerciseAPIString(query):
     url = "https://exercisedb.dev/api/v1/exercises/search"
@@ -30,10 +30,23 @@ def searchExerciseAPIID(id):
 
     return result
 
+def loadWorkoutsFromSchedule(scheduleID):
+
+    print("loading schedule days for scheduleID: ", scheduleID, "userID: ", current_user.id)
+
+    workoutDays = []
+    for i in range(1,8):
+        scheduleDay = ScheduleDays.query.filter_by(scheduleID=scheduleID, day=i).first()
+        if scheduleDay:
+            workoutDays.append(Workouts.query.filter_by(id=scheduleDay.workoutID).first())
+        else:
+            workoutDays.append(Workouts.query.filter_by(id=1).first())
+    
+    return workoutDays
 
 @loginManager.user_loader
 def loaduser(userID):
-    print('loading user')
+    print("loading user")
     return Users.query.get(int(userID))
 
 @bp.route('/', methods=['GET', 'POST'])
@@ -45,59 +58,53 @@ def home():
 @bp.route('/profile', methods=['GET', 'POST'])
 @login_required
 def profile():
-    print('profile')
+    print("profile")
     return render_template('profile.html')
 
 @bp.route('/editSchedule', methods=['GET', 'POST'])
 @login_required
 def editSchedule():
 
-    print('Loading edit-schedule ...')
+    print("Loading edit-schedule ...")
 
     form = SearchExercise()
     schedule = Schedules.query.filter_by(id=current_user.currentScheduleID).first()
-    scheduleDays = []
 
     if not schedule:
-        print('no schedule found for user, setting to default schedule')
+        print("no schedule found for user, setting to default schedule")
         Users.query.filter_by(id=current_user.id).first().currentScheduleID = 1
         db.session.commit()
 
-    print('schedule found for user')
-    for i in range(1,8):
-        scheduleDay = ScheduleDays.query.filter_by(scheduleID=schedule.id, day=i).first()
-        if scheduleDay:
-            scheduleDays.append(Workouts.query.filter_by(id=scheduleDay.workoutID).first())
-        else:
-            scheduleDays.append(Workouts.query.filter_by(id=1).first())
+    print("schedule found for user")
+    workoutDays = loadWorkoutsFromSchedule(schedule.id)
 
     if form.is_submitted():
-        print('form submitted')
-        print('searching for exercises ...')
+        print("form submitted")
+        print("searching for exercises ...")
 
         response = searchExerciseAPIString(form.search.data)['data']
 
         if response:
-            print('exercises found')
-            return render_template('edit-schedule.html', title='Edit-Schedule', form=form, query=form.search.data, results=response, schedule=schedule, scheduleDays=scheduleDays, daysOfTheWeek=daysOfTheWeek)
+            print("exercises found")
+            return render_template('edit-schedule.html', title='Edit-Schedule', form=form, query=form.search.data, results=response, schedule=schedule, workoutDays=workoutDays, daysOfTheWeek=daysOfTheWeek)
 
-    return render_template('edit-schedule.html', title='Edit-Schedule', form=form, schedule=schedule, scheduleDays=scheduleDays, daysOfTheWeek=daysOfTheWeek) 
+    return render_template('edit-schedule.html', title='Edit-Schedule', form=form, schedule=schedule, workoutDays=workoutDays, daysOfTheWeek=daysOfTheWeek) 
 
 @bp.route('/signIn', methods=['GET', 'POST'])
 def signIn():
     form = LoginForm()
     if form.validate_on_submit():
-        print('form validated')
-        print('searching for user')
+        print("form validated")
+        print("searching for user")
         user = Users.query.filter_by(username=form.username.data).first()
         if user:
-            print('user found')
+            print("user found")
             if bcrypt.check_password_hash(user.password, form.password.data):
-                print('password correct, logging in')
+                print("password correct, logging in")
                 login_user(user)
                 return redirect(url_for('main.home'))
             else:
-                print('password incorrect')
+                print("password incorrect")
                 flash('Invalid username or password')
     return render_template('signIn.html', title='Sign In', form=form)
 
@@ -105,8 +112,8 @@ def signIn():
 def signUp():
     form = SignUpForm()
     if form.validate_on_submit():
-        print('form validated')
-        print('creating user')
+        print("form validated")
+        print("creating user")
         hashedPassword = bcrypt.generate_password_hash(form.password.data).decode('utf8')
         newUser = Users(username=form.username.data, password=hashedPassword, email=form.email.data, firstName=form.firstName.data, lastName=form.lastName.data, currentScheduleID=1)
         db.session.add(newUser)
@@ -118,16 +125,16 @@ def signUp():
 @bp.route('/signOut', methods=('GET', 'POST'))
 @login_required
 def signOut():
-    print('logging out')
+    print("logging out")
     logout_user()
     return redirect(url_for('main.signIn'))
 
 @bp.route('/admin', methods=['GET', 'POST'])
 def admin():
-    print('admin')
+    print("admin")
     return render_template('admin.html')
 
 @bp.route('/manage', methods=['GET', 'POST'])
 def manage():
-    print('manage')
+    print("manage")
     return render_template('manage.html')
