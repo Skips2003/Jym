c1fe7bb722d01d78eb50a92950cc3c6dae8745a4
Skips2003 <jonnyforbes0805@gmail.com>
2026-03-24 15:49:49 +0000

    removed flask-meld as it is deprecated and not compatible with the latest version of Flask.

--- app/__init__.py
+++ app/__init__.py
@@ -3,17 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 from config import Config
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from flask_meld import Meld
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 loginManager = LoginManager()
-meld = Meld()
 
 def createApp(configClass = Config):
     app = Flask(__name__)
     app.config.from_object(configClass)
-    meld.init_app(app)
     db.init_app(app)
     bcrypt.init_app(app)
 
