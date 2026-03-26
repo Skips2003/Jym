1890a99bb11cfeb85f953dafc59342700c0c3e5f
Skips2003 <jonnyforbes0805@gmail.com>
2026-03-26 16:48:34 +0000

    Schedules,Workouts,Exercises Databases all migrated to MongoDB!
    TBH this doesn't really change much for functionality but i just wanted to learn some noSQL databases, Users Database still on postgres and will stay there

--- app/__init__.py
+++ app/__init__.py
@@ -3,16 +3,19 @@ from flask_sqlalchemy import SQLAlchemy
 from config import Config
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_pymongo import PyMongo
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 loginManager = LoginManager()
+mongo = PyMongo()
 
 def createApp(configClass = Config):
     app = Flask(__name__)
     app.config.from_object(configClass)
     db.init_app(app)
     bcrypt.init_app(app)
+    mongo.init_app(app)
 
     loginManager.init_app(app)
     loginManager.login_view = "main.signIn"
--- app/models.py
+++ app/models.py
@@ -18,88 +18,10 @@ class Users(db.Model, UserMixin):
     bio: so.Mapped[str] = so.mapped_column(sa.String(300), default='Default-Bio')
     followers: so.Mapped[int] = so.mapped_column(default=0)
     following: so.Mapped[int] = so.mapped_column(default=0)
-    currentScheduleID: so.Mapped[int] = so.mapped_column(sa.ForeignKey("Schedules.id"), default=None)
+    currentScheduleID: so.Mapped[str] = so.mapped_column(default="69c44bc4735131196e47244d")
     sessionsInRow: so.Mapped[int] = so.mapped_column(default=0)
     bigThreeTotal: so.Mapped[int] = so.mapped_column(default=0)
     quickStatThree: so.Mapped[int] = so.mapped_column(default=0)
     benchPress: so.Mapped[int] = so.mapped_column(default=0)
     deadLift: so.Mapped[int] = so.mapped_column(default=0)
     squat: so.Mapped[int] = so.mapped_column(default=0)
-
-class Schedules(db.Model):
-
-    __tablename__ = 'Schedules'
-
-    id: so.Mapped[int] = so.mapped_column(primary_key=True, unique=True)
-    name: so.Mapped[str] = so.mapped_column(sa.String(120), unique=True)
-    description: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
-    public: so.Mapped[bool] = so.mapped_column(default=True)
-
-    days = db.relationship('ScheduleDays')
-
-class Workouts(db.Model):
-
-    __tablename__ = 'Workouts'
-
-    id: so.Mapped[int] = so.mapped_column(primary_key=True, unique=True)
-    name: so.Mapped[str] = so.mapped_column(sa.String(120), unique=True)
-    description: so.Mapped[str] = so.mapped_column(sa.String(120), unique=True)
-    public: so.Mapped[bool] = so.mapped_column(default=True)
-
-    days = db.relationship('ScheduleDays')
-
-class Exercises(db.Model):
-
-    __tablename__ = 'Exercises'
-
-    id: so.Mapped[int] = so.mapped_column(primary_key=True, unique=True)
-    exerciseSearchID: so.Mapped[str]
-    reps: so.Mapped[int] = so.mapped_column(default=0)
-    sets: so.Mapped[int] = so.mapped_column(default=0)
-    weight: so.Mapped[int] = so.mapped_column(default=0)
-
-class ScheduleDays(db.Model):
-
-    __tablename__ = 'ScheduleDays'
-
-    scheduleID: so.Mapped[int] = so.mapped_column(sa.ForeignKey("Schedules.id"), primary_key=True)
-    workoutID: so.Mapped[int] = so.mapped_column(sa.ForeignKey("Workouts.id"), primary_key=True)
-    day: so.Mapped[int] = so.mapped_column(default=0)
-
-    schedule = db.relationship('Schedules')
-    workout = db.relationship('Workouts')
-
-class WorkoutExercises(db.Model):
-
-    __tablename__ = 'WorkoutExercises'
-
-    workoutID: so.Mapped[int] = so.mapped_column(sa.ForeignKey("Workouts.id"), primary_key=True)
-    exerciseID: so.Mapped[int] = so.mapped_column(sa.ForeignKey("Exercises.id"), primary_key=True)
-
-class SavedSchedules(db.Model):
-
-    __tablename__ = 'SavedSchedules'
-
-    userID: so.Mapped[int] = so.mapped_column(sa.ForeignKey("Users.id"), primary_key=True)
-    scheduleID: so.Mapped[int] = so.mapped_column(sa.ForeignKey("Schedules.id"), primary_key=True)
-
-class BookmarkedSchedules(db.Model):
-
-    __tablename__ = 'BookmarkedSchedules'
-
-    userID: so.Mapped[int] = so.mapped_column(sa.ForeignKey("Users.id"), primary_key=True)
-    scheduleID: so.Mapped[int] = so.mapped_column(sa.ForeignKey("Schedules.id"), primary_key=True)
-
-class SavedWorkouts(db.Model):
-
-    __tablename__ = 'SavedWorkouts'
-
-    userID: so.Mapped[int] = so.mapped_column(sa.ForeignKey("Users.id"), primary_key=True)
-    workoutID: so.Mapped[int] = so.mapped_column(sa.ForeignKey("Schedules.id"), primary_key=True)
-
-class BookmarkedWorkouts(db.Model):
-
-    __tablename__ = 'BookmarkedWorkouts'
-
-    userID: so.Mapped[int] = so.mapped_column(sa.ForeignKey("Users.id"), primary_key=True)
-    workoutID: so.Mapped[int] = so.mapped_column(sa.ForeignKey("Schedules.id"), primary_key=True)
